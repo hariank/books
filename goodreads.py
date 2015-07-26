f39716c1b56b6c87745b03f372b9a1b6c84b4b7b
hariank <hariank3k@gmail.com>
2015-07-25 22:21:46 -0400

    pull goodreads data to json file

---
+++ goodreads.py
@@ -0,0 +1,31 @@
+import requests
+import xml.etree.ElementTree as ET
+import json
+
+url = "https://www.goodreads.com/review/list/45141963?format=xml&key=KEY&v=2"
+key = "yFhXhuMkI2MvcepEd0ipMw"
+
+r = requests.get(url.replace('KEY', key))
+root = ET.fromstring(r.text.encode('utf-8'))
+
+book_data = {}
+book_data['currently-reading'] = []
+book_data['read'] = []
+book_data['to-read'] = []
+
+for review in root.iter('review'):
+
+    book_obj = {}
+    book = review.find('book')
+    book_obj['title'] = book.find('title').text
+    book_obj['image_url'] = book.find('image_url').text
+    book_obj['link'] = book.find('link').text
+
+    #print(book_obj)
+    shelf_type = review.find('shelves').find('shelf').get('name')
+
+    book_data[shelf_type].append(book_obj)
+
+json_data = json.dumps(book_data)
+with open('book_data.json', 'w') as outfile:
+    json.dump(json_data, outfile)
--- js/Shelf.js
+++ js/Shelf.js
@@ -5,23 +5,40 @@ var Shelf = React.createClass({
 	},
 	
 	loadBooks: function() {
-		this.setState({data: ['test']})
+        $.get(this.props.url, function(result) {
+            console.log(result);
+        }.bind(this));
+        //$.ajax({
+            //url: this.props.url,
+            //dataType: 'xml',
+            //cache: false,
+            //success: function(data) {
+                //this.setState({data: data});
+            //}.bind(this),
+            //error: function(xhr, status, err) {
+                ////console.error(this.props.url, status, err.toString());
+            //}.bind(this)
+        //});
+		this.setState({data: ['test']});
 	},
 
 	componentDidMount: function() {
 		this.loadBooks();
 	},
 
-	render: function() {
-		return (
-			<div className="shelf">
-                <p> test </p>
-			</div>
-		);
-	}
+    render: function() {
+        return (
+            <div classname="shelf">
+                <p> url: { this.props.data } </p>
+            </div>
+        );
+    }
 });
 
+var url = "https://www.goodreads.com/review/list/45141963?format=xml&key=KEY&v=2"
+var key = "yFhXhuMkI2MvcepEd0ipMw"
+
 React.render(
-    <Shelf />,
+    <Shelf url={url.replace('KEY', key)} />,
     document.getElementById('content')
 );
