82cdf4dca963ebe56602a6cd0abf287c492d3f3e
hariank <hariank3k@gmail.com>
2015-07-27 23:17:29 -0400

    call goodreads api directly (except for links)

--- js/Shelf.js
+++ js/Shelf.js
@@ -6,7 +6,24 @@ var Shelf = React.createClass({
 	
 	loadBooks: function() {
         $.get(this.props.url, function(result) {
-            this.setState({data: JSON.parse(result)});
+            var book_data = {};
+            book_data['currently-reading'] = []
+            book_data['read'] = []
+            book_data['to-read'] = []
+
+            var titleind = 4, imageind = 5;
+            $(result).find('review').each(function(index, rev) {
+                var book_obj = {}
+
+                var book = $(rev).find('book').children();
+                book_obj['title'] = book[titleind].innerText;
+                book_obj['image_url'] = book[imageind].innerText;
+
+                var shelf = $(rev).find('shelves').children()[0].attributes[0].value;
+                book_data[shelf].push(book_obj);
+            });
+
+            this.setState({data: book_data});
         }.bind(this));
 	},
 
@@ -52,13 +69,16 @@ var Book = React.createClass({
                 <a href={book.link} target="_blank">
                     <img src={book.image_url} width="80" />
                 </a>
-                {/*<p> {book.title} </p>*/}
             </li>
         );
     }
 })
 
-var url = "book_data.json"
+//var url = "book_data.json"
+var cors = "http://crossorigin.me/";
+var url_part = "https://www.goodreads.com/review/list/45141963?format=xml&key=KEY&v=2";
+var key = "yFhXhuMkI2MvcepEd0ipMw";
+var url = cors + url_part.replace('KEY', key);
 
 React.render(
     <Shelf url={url} />,
